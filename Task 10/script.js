b29d8f92cba0c621fd54983abbe920ab8f5be306
Rashkele <ramune.cincikiene@gmail.com>
2022-05-16 19:33:03 +0300

    First commit

---
+++ Task 10/script.js
@@ -0,0 +1,28 @@
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
+
+let a = composition(1, 4);
+function composition(one, four) {
+    return one + four;
+};
+
+let b = division(4, 2);
+function division(four, two) {
+    return four / two;
+};
+
+let c = substraction(3, 2);
+function substraction(three, two) {
+    return three - two;
+};
+
+let d = multiplication(5, 2);
+function multiplication (five, two) {
+    return five * two;
+};
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
